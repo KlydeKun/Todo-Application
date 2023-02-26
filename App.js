a2814bdfa2764a446d2241c561fe473688536800
klyde <kylelerio15@gmail.com>
2023-02-26 18:15:59 +0800

    first Commit

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "./components/Header";
+import Task from "./components/Task";
+import AddTask from "./components/AddTask";
+import 'react-native-get-random-values';
+import { v4 as uuid } from 'uuid';
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+    { task: "HTML I", done: true, id: "1" },
+    { task: "CSS", done: true, id: "2" },
+    { task: "Responsive design", done: true, id: "3" },
+  ]);
+  const addTask = (text) => {
+    setTasks((prevTasks) => {
+      return [{ task: text, id: uuid() }
+        , ...prevTasks];
+    });
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <AddTask addTask={addTask} />
+        <View style={styles.list}>
+          <FlatList
+            data={tasks}
+            renderItem={({ item }) => (
+            <Task item={item} />
+            )}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 30,
+  },
+  list: {
+    marginTop: 30,
   },
 });
